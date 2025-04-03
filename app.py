2cb6ef211e7e23268191d541461a06c703f0e838
MrNoahMango <tralalamango@gmail.com>
2025-04-03 15:18:09 +0200

    add index and credits

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 
+index = st.Page("index.py", title="Home")
+
 mango_profile = st.Page("profiles/mango.py", title="Mango")
 julius_profile = st.Page("profiles/julius.py", title="Julius")
 
+credits_page = st.Page("credits.py", title="Credits")
+
 pg = st.navigation(
     {
-        "Profiles": [mango_profile, julius_profile]
+        "": [index],
+        "Profiles": [mango_profile, julius_profile],
+        "Misc": [credits_page]
     }
 )
 
